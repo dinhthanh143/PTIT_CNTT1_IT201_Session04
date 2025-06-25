19ddb3ef8bb0a23d5542630758202aa15d527c2e
dinhthanh143 <neonlimea@gmail.com>
2025-06-25 20:21:40 +0700

    Update PTIT_CNTT1_IT201_Session04_Bai09.c

--- PTIT_CNTT1_IT201_Session04/PTIT_CNTT1_IT201_Session04_Bai09.c
+++ PTIT_CNTT1_IT201_Session04/PTIT_CNTT1_IT201_Session04_Bai09.c
@@ -2,7 +2,49 @@
 #include<string.h>
 #include<math.h>
 #include<stdlib.h>
+int *insertArray(int *arr, int *size) {
+   while (1) {
+      printf("moi nhap so luong phan tu: ");
+      scanf("%d", size);
+      if (*size<1 && *size<100) {
+         printf("so luong phan tu phai lon hon 0 va be hon 100.\n");
+      }else {
+         break;
+      }
+   }
+   arr = (int*)realloc(arr, *size*sizeof(arr));
+   for (int i = 0; i<*size; i++) {
+      printf("\nmoi nhap phan tu thu %d:", i+1);
+      scanf("%d", &arr[i]);
+   }
+   return arr;
+}
+int searchArray(int arr[], int size,int value) {
+  int left = 0;
+   int right = size -1;
+   while (left<=right) {
+      int mid = (left+right)/2;
+      if (arr[mid] == value) {
+         return mid;
+      }else if (arr[mid]>value) {
+         right = mid -1;
+
+      }else {
+         left = mid +1;
+      }
+   }
+   return -1
+}
 int main(){
+   int size;
+   int *arr = NULL;
+   int value;
+   printf("\nmoi nhap phan tu muon tim: ");
+   scanf("%d", &value);
+   int check = 0;
+   while () {
+
+   }
    
 
    return 0;
